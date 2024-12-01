40289d2ad1ff7600e8cdeb96e6d47df538740726
Ernandy Lucena <83795827+ErnandyLucena@users.noreply.github.com>
2024-12-01 00:00:08 -0300

    Add files via upload
    add experiencia container

--- js/script.js
+++ js/script.js
@@ -15,41 +15,36 @@ const nextButton = document.querySelector('.carousel__btn.next');
 let currentIndex = 0;
 let intervalId;
 
-// Função para atualizar o slide
-function updateSlide(index) {
-  slides.style.transform = `translateX(-${index * 100}%)`;
+var testimonialItems = document.querySelectorAll(".item label");
+var timer;
+function cycleTestimonials(index) {
+   timer = setTimeout(function() {
+    var evt;
+    if (document.createEvent){
+      //If browser = IE, then polyfill
+      evt = document.createEvent('MouseEvent');
+      evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    } else {
+      //If Browser = modern, then create new MouseEvent
+      evt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            clientX: 20
+          });
+    }
+    var ele = "." + testimonialItems[index].className;
+    var ele2 = document.querySelector(ele)
+    ele2.dispatchEvent(evt);
+    index++; // Increment the index
+    if (index >= testimonialItems.length) {
+      index = 0; // Set it back to `0` when it reaches `3`
+    }
+    cycleTestimonials(index); // recursively call `cycleTestimonials()`
+    document.querySelector(".testimonials").addEventListener("click", function() {
+      clearTimeout(timer); //stop the carousel when someone clicks on the div
+    });
+  }, 2000); //adjust scroll speed in miliseconds
 }
-
-// Função para ir ao próximo slide
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slideCount;
-  updateSlide(currentIndex);
-}
-
-// Função para ir ao slide anterior
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + slideCount) % slideCount;
-  updateSlide(currentIndex);
-}
-
-// Eventos dos botões
-nextButton.addEventListener('click', () => {
-  clearInterval(intervalId);
-  nextSlide();
-  startAutoSlide();
-});
-
-prevButton.addEventListener('click', () => {
-  clearInterval(intervalId);
-  prevSlide();
-  startAutoSlide();
-});
-
-// Função para iniciar o deslizamento automático
-function startAutoSlide() {
-  intervalId = setInterval(nextSlide, 5000);
-}
-
-// Iniciar deslizamento automático
-startAutoSlide();
-
+//run the function
+cycleTestimonials(0);
